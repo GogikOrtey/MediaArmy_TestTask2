8daff5bd76a44bd063864ce12ee811f712b7a3fa
Gogik <gog.ortey@yandex.ru>
2025-03-01 19:34:35 +0500

    11 Звезда генерируется в случайном месте

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     diamond.id = 'diamond';
     document.body.appendChild(diamond);
 
+    // Стартовые стили для создаваемого объекта
     const styles = {
         width: '0',
         height: '0',
@@ -12,20 +13,30 @@ document.addEventListener("DOMContentLoaded", function() {
         borderBottomColor: 'rgba(255, 255, 255, 1)',
         borderLeftColor: 'rgba(255, 255, 255, 1)',
         transform: 'rotate(45deg)',
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        marginLeft: '-2px', /* Половина ширины */
-        marginTop: '-2px' /* Половина высоты */
+        position: 'absolute'
     };
 
+    // Применяем стили для объекта
     Object.assign(diamond.style, styles);
+
+    // Генерация случайных координат
+    function getRandomPosition() {
+        const x = Math.random() * window.innerWidth;
+        const y = Math.random() * window.innerHeight;
+        return {x, y};
+    }
+
+    // Установка объекта в случайное место
+    const {x, y} = getRandomPosition();
+    diamond.style.left = `${x}px`;
+    diamond.style.top = `${y}px`;
 
     // Функция для изменения альфа-канала
     let step = 0;
     const steps = [1, 0.9, 0.8, 0.7, 0.6, 0.7, 0.8, 0.9, 1];
     const interval = 200; // 200 миллисекунд
 
+    // Изменяет видимость звезды, в зависимости от № шага 
     function changeOpacity() {
         const alpha = steps[step];
         diamond.style.borderTopColor = `rgba(255, 255, 255, ${alpha})`;
@@ -38,3 +49,19 @@ document.addEventListener("DOMContentLoaded", function() {
     // Устанавливаем таймер
     setInterval(changeOpacity, interval);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
