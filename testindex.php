67951c50d31fdfff44008b30c42703986c0edc24
Gogik <gog.ortey@yandex.ru>
2025-03-01 23:36:54 +0500

    20 Разобрался с запуском php файлов

---
+++ testindex.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ru">
+<body>
+    <p>
+        <?php
+            echo 2+3;
+        ?>
+    </p>    
+</body>
+</html>
---
+++ Запуск сервера php.cmd
@@ -0,0 +1,4 @@
+start "" "http://localhost:8000/index.php"
+php -S localhost:8000
+
+
