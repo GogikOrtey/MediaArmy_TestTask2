f1d8e679a66bda224dfe660c6d8cc4f303d407fb
Gogik <gog.ortey@yandex.ru>
2025-03-01 17:38:50 +0500

    07 Дополнил обработчик - теперь он корректно работает с повреждёнными строками

--- Task 2/main.php
+++ Task 2/main.php
@@ -20,7 +20,8 @@
 
 
 // Определяем путь к файлу
-$file = 'DataBase.csv';
+// $file = 'DataBase.csv';
+$file = 'DataBase_damaged.csv';
 
 // Проверяем, существует ли файл
 if (!file_exists($file)) {
@@ -37,14 +38,52 @@ if ($handle === false) {
 // Устанавливаем разделитель табуляции
 $separator = "\t";
 
+// Стандартное количество колонок а записи
+$expectedColumns = 3;
+
+// Печатаем ли мы ошибки, произошедшие в результате обработки строк?
+$isEchoErrors = true;
+
+// Количество ошибок, возникших при загрузке значений из входного файла
+$encounteredErrorsCounter = 0;
+
+// Общее количество загруженных строк
+$allCounterForStr = 0;
+
 // Массив для хранения данных
 $data = [];
 
 // Читаем файл строка за строкой 
 while (($row = fgetcsv($handle, 0, $separator)) !== false) {
     // print_r($row);
+
+    $allCounterForStr++;
+
+    if (count($row) !== $expectedColumns) {
+        if($isEchoErrors) {
+            echo "Ошибка: Неправильное количество колонок в строке:\n"; 
+        }
+        print_r($row);
+        $encounteredErrorsCounter++;
+        continue;
+    }
+
+    // Проверяем, не пустые ли значения в колонках
+    foreach ($row as $index => $value) {
+        if (empty($value)) {
+            if($isEchoErrors) {
+                echo "Ошибка: Пустое значение в строке:\n";
+            }
+            print_r($row);
+            $encounteredErrorsCounter++;
+            continue 2; // Пропускаем строку
+        }
+    }
+
     $data[] = $row;
 }
+
+// Важно не забывать, что первый элемент массива содержит заголовки строк, а не данные
 
 // Закрываем файл
 fclose($handle);
@@ -52,6 +91,14 @@ fclose($handle);
 // Выводим массив
 print_r($data);
 
+if($encounteredErrorsCounter > 0) {
+    echo("\nКоличество ошибок, произошедших при загрузке данных из входного файла: $encounteredErrorsCounter");
+}
+
+$resultCountLoadDataStr = $allCounterForStr - 1 - $encounteredErrorsCounter;
+// -1, потому что первая запись - это заголовки строк
+
+echo("\n\nОбщее количество загруженных записей: $resultCountLoadDataStr");
 
 
 
